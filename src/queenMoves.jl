18f28e4005baca24b6209769e5620d3123afe93a
Jeremy <j.d.revell1@ncl.ac.uk>
2019-11-16 21:35:07 +0000

    type dispatch on queenMoves()

--- src/queenMoves.jl
+++ src/queenMoves.jl
@@ -1,1 +1,2 @@
+# To generate queen moves, simply take the union of all possible rook moves + all possible bishop moves from that square.
 queenMoves(sqr::T, occupied::UInt) where T <: Union{Int, UInt} = rookMoves(sqr, occupied) | bishopMoves(sqr, occupied)
