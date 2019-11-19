d0928c93962056ec7cf90b106566947eb3ad8547
Jeremy <j.d.revell1@ncl.ac.uk>
2019-11-19 18:44:56 +0000

    Alternate sliding table lookup

--- src/bishopMoves.jl
+++ src/bishopMoves.jl
@@ -6,7 +6,9 @@ bishopMove_NW(bishop::UInt64) = ~FILE_H & (bishop << 9)
 const BISHOP_MOVE_FUNCTIONS = @SVector Function[bishopMove_NE, bishopMove_SE, bishopMove_SW, bishopMove_NW]
 
 # initialise the tables on startup
-const BISHOP_TABLE = initSlidingTable(Vector{UInt}(undef, 5248), BISHOP_MAGICS, BISHOP_MOVE_FUNCTIONS)
+#const BISHOP_TABLE = initSlidingTable(Vector{UInt}(undef, 5248), BISHOP_MAGICS, BISHOP_MOVE_FUNCTIONS)
+const BISHOP_TABLE = initSlidingTable(Vector{Vector{UInt}}(undef, 64), BISHOP_MAGICS, BISHOP_MOVE_FUNCTIONS)
 
-bishopMoves(sqr::Int, occupied::UInt64) = @inbounds BISHOP_TABLE[tableIndex(occupied, BISHOP_MAGICS[sqr])]
+#bishopMoves(sqr::Int, occupied::UInt64) = @inbounds BISHOP_TABLE[tableIndex(occupied, BISHOP_MAGICS[sqr])]
+bishopMoves(sqr::Int, occupied::UInt64) = @inbounds BISHOP_TABLE[sqr][subIndex(occupied, BISHOP_MAGICS[sqr])]
 bishopMoves(sqr::UInt, occupied::UInt64) = getBishopMoves(getSquare(sqr), occupied)
--- src/slidingMoves.jl
+++ src/slidingMoves.jl
@@ -23,12 +23,25 @@ function sliderMoves(sqr_bb::UInt64, occupied::UInt64, moveDirections::SArray{Tu
 end
 
 # generate the sliding tables, by creating the indexes and all possible occupancy masks.
-function initSlidingTable(table::Vector{UInt}, magics::SArray{Tuple{64}, Magic, 1, 64}, moveDirections::SArray{Tuple{4},Function,1,4})
+# function initSlidingTable(table::Vector{UInt}, magics::SArray{Tuple{64}, Magic, 1, 64}, moveDirections::SArray{Tuple{4},Function,1,4})
+# 	for sqr in 1:64
+# 		occupied = zero(UInt)
+# 		@inbounds for i in 1:(1 << count_ones(magics[sqr].mask))
+# 			idx = tableIndex(occupied, magics[sqr])
+# 			table[idx] = sliderMoves(getBitboard(sqr), occupied, moveDirections)
+# 			occupied = (occupied - magics[sqr].mask) & magics[sqr].mask
+# 		end
+# 	end
+# 	return table
+# end
+
+function initSlidingTable(table::Vector{Vector{UInt}}, magics::SArray{Tuple{64}, Magic, 1, 64}, moveDirections::SArray{Tuple{4},Function,1,4})
 	for sqr in 1:64
 		occupied = zero(UInt)
+		table[sqr] = Vector{UInt}(undef, 2^count_ones(magics[sqr].mask))
 		@inbounds for i in 1:(1 << count_ones(magics[sqr].mask))
-			idx = tableIndex(occupied, magics[sqr])
-			table[idx] = sliderMoves(getBitboard(sqr), occupied, moveDirections)
+			idx = subIndex(occupied, magics[sqr])
+			table[sqr][idx] = sliderMoves(getBitboard(sqr), occupied, moveDirections)
 			occupied = (occupied - magics[sqr].mask) & magics[sqr].mask
 		end
 	end
