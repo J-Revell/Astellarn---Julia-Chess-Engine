c03c080c7bae201726270e12fb9a8b521919e907
Jeremy <j.d.revell1@ncl.ac.uk>
2019-11-27 12:14:39 +0000

    add updated runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,17 +1,76 @@
 using ChessProject
 using Test
 
-# Test case 1. This case is chosen to identify a case where there is *only* one legal move for the king, which is to capture the black queen.
-println("Test Case 1:")
-test_board_1 = Board(UInt8[0x11, 0x09, 0x0d, 0x00, 0x15, 0x0d, 0x09, 0x00, 0x05, 0x05, 0x16, 0x05, 0x00, 0x05, 0x05, 0x00, 0x00, 0x00, 0x19,
-    0x00, 0x05, 0x00, 0x00, 0x11, 0x00, 0x00, 0x05, 0x00, 0x00, 0x00, 0x00, 0x05, 0x06, 0x06, 0x00, 0x06, 0x00, 0x00, 0x06, 0x00, 0x00, 0x00,
-    0x0a, 0x00, 0x0e, 0x06, 0x00, 0x00, 0x00, 0x00, 0x06, 0x00, 0x06, 0x00, 0x00, 0x06, 0x00, 0x12, 0x00, 0x1a, 0x00, 0x0e, 0x0a, 0x12],
-    [0x0094204b84106b00, 0x4000040000000042, 0x2000100000000024, 0x8200000000800001, 0x0000000000000410, 0x0800000000040000],
-    [0x0000000084946b77, 0xea94344b00000400], UInt(1) <<24 , 0x01, 0x0f, 0x00, zero(UInt16))
-# in this test case, we expect 5 candidate moves to be generated, all from the king.
-ml = MoveList(200)
-gen_moves!(ml, test_board_1)
-@test length(ml) == 5
-@test isCheckmate(test_board_1) == false
-@test isStalemate(test_board_1) == false
-@test isDrawByMaterial(test_board_1) == false
+# First test perft on known positions for common bugs
+# https://www.chessprogramming.org/Perft_Results
+
+@testset "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1" begin
+    fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+    b = importfen(fen)
+    @test cancastlekingside(b) == true
+    @test cancastlekingside(b, BLACK) == true
+    @test cancastlequeenside(b) == true
+    @test cancastlequeenside(b, BLACK) == true
+    @test ischeck(b) == false
+    @test b.enpass == zero(UInt8)
+    @test perft(b, 1) == 20
+    @test perft(b, 2) == 400
+    @test perft(b, 3) == 8902
+    @test perft(b, 4) == 197281
+    @test perft(b, 5) == 4865609
+    @test perft(b, 6) == 119060324
+end
+
+@testset "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -" begin
+    fen = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq -"
+    b = importfen(fen)
+    @test cancastlekingside(b) == true
+    @test cancastlekingside(b, BLACK) == true
+    @test cancastlequeenside(b) == true
+    @test cancastlequeenside(b, BLACK) == true
+    @test ischeck(b) == false
+    @test b.enpass == zero(UInt8)
+    @test perft(b, 1) == 48
+    @test perft(b, 2) == 2039
+    @test perft(b, 3) == 97862
+    @test perft(b, 4) == 4085603
+    @test perft(b, 5) == 193690690
+    #@test perft(b, 6) == 8031647685
+end
+
+@testset "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - " begin
+    fen = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - "
+    b = importfen(fen)
+    @test cancastlekingside(b) == false
+    @test cancastlekingside(b, BLACK) == false
+    @test cancastlequeenside(b) == false
+    @test cancastlequeenside(b, BLACK) == false
+    @test ischeck(b) == false
+    @test b.enpass == zero(UInt8)
+    @test perft(b, 1) == 14
+    @test perft(b, 2) == 191
+    @test perft(b, 3) == 2812
+    @test perft(b, 4) == 43238
+    @test perft(b, 5) == 674624
+    @test perft(b, 6) == 11030083
+end
+
+@testset "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1" begin
+    fen1 = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1"
+    fen2 = "r2q1rk1/pP1p2pp/Q4n2/bbp1p3/Np6/1B3NBn/pPPP1PPP/R3K2R b KQ - 0 1"
+    b1 = importfen(fen1)
+    b2 = importfen(fen2)
+    @test cancastlekingside(b1, BLACK) == true
+    @test cancastlequeenside(b1, BLACK) == true
+    @test cancastlekingside(b1, WHITE) == false
+    @test cancastlequeenside(b1, WHITE) == false
+    @test cancastlekingside(b2, WHITE) == true
+    @test cancastlequeenside(b2, WHITE) == true
+    @test cancastlekingside(b2, BLACK) == false
+    @test cancastlequeenside(b2, BLACK) == false
+    @test perft(b1, 1) == perft(b2, 1) == 6
+    @test perft(b1, 2) == perft(b2, 2) == 264
+    @test perft(b1, 3) == perft(b2, 3) == 9467
+    @test perft(b1, 4) == perft(b2, 4) == 422333
+    @test perft(b1, 5) == perft(b2, 5) == 15833292
+end
