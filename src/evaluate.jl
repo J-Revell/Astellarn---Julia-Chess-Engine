d3ea92c177aeface6a16007c4ad53df7a914340d
Jeremy <j.d.revell1@ncl.ac.uk>
2019-12-21 22:12:12 +0000

    evaluation breakdown and pin values

--- src/evaluate.jl
+++ src/evaluate.jl
@@ -73,79 +73,170 @@ const PVALS = @SVector [100, 300, 300, 500, 900, 2500]
 Naive evaluation function to get the code development going.
 """
 function evaluate(board::Board)
-    # if isdraw(board)
-    #     return 0
-    # end
-    # if ischeckmate(board)
-    #     return -10000
-    # end
-    w_pawns     = board[WHITEPAWN]      ; b_pawns   = board[BLACKPAWN]
-    w_knights   = board[WHITEKNIGHT]    ; b_knights = board[BLACKKNIGHT]
-    w_bishops   = board[WHITEBISHOP]    ; b_bishops = board[BLACKBISHOP]
-    w_rooks     = board[WHITEROOK]      ; b_rooks   = board[BLACKROOK]
-    w_queens    = board[WHITEQUEEN]     ; b_queens  = board[BLACKQUEEN]
-    w_king      = board[WHITEKING]      ; b_king    = board[BLACKKING]
+    eval = 0
 
-    pval = 100 ; nval = 300; bval = 300; rval = 500; qval = 900; kval = 2500
+    eval += evaluate_pawns(board)
+    eval += evaluate_knights(board)
+    eval += evaluate_bishops(board)
+    eval += evaluate_rooks(board)
+    eval += evaluate_queens(board)
+    eval += evaluate_kings(board)
 
-    position_eval = 0
+    eval += evaluate_pins(board)
 
-    # could use matrix operations for this
-    material_eval = pval*count(w_pawns) + nval*count(w_knights) + bval*count(w_bishops) + rval*count(w_rooks) + qval*count(w_queens) + kval*count(w_king)
-    material_eval -= (pval*count(b_pawns) + nval*count(b_knights) + bval*count(b_bishops) + rval*count(b_rooks) + qval*count(b_queens) + kval*count(b_king))
-
-    # pawn evaluations
-    for pawn in w_pawns
-        position_eval += PAWN_EVAL_TABLE[pawn]
-    end
-    for pawn in b_pawns
-        position_eval -= PAWN_EVAL_TABLE[65-pawn]
-    end
-
-    # knight evaluations
-    for knight in w_knights
-        position_eval += KNIGHT_EVAL_TABLE[knight]
-    end
-    for knight in b_knights
-        position_eval -= KNIGHT_EVAL_TABLE[65-knight]
-    end
-
-    # bishop evaluations
-    for bishop in w_bishops
-        position_eval += BISHOP_EVAL_TABLE[bishop]
-    end
-    for bishop in b_bishops
-        position_eval -= BISHOP_EVAL_TABLE[65-bishop]
-    end
-
-    # rook evaluations
-    for rook in w_rooks
-        position_eval += ROOK_EVAL_TABLE[rook]
-    end
-    for rook in b_rooks
-        position_eval -= ROOK_EVAL_TABLE[65-rook]
-    end
-
-    # queen evaluations
-    for queen in w_queens
-        position_eval += QUEEN_EVAL_TABLE[queen]
-    end
-    for queen in b_queens
-        position_eval -= QUEEN_EVAL_TABLE[65-queen]
-    end
-
-    # king evaluations
-    for king in w_king
-        position_eval += KING_EVAL_TABLE[king]
-    end
-    for king in b_king
-        position_eval -= KING_EVAL_TABLE[65-king]
-    end
-
-    eval = material_eval + position_eval
     if board.turn == WHITE
         return eval
     else
         return -eval
     end
+end
+
+
+function evaluate_pawns(board::Board)
+    w_pawns = board[WHITEPAWN]
+    b_pawns = board[BLACKPAWN]
+    pval = PVALS[1]
+
+    material_eval = 0
+    position_eval = 0
+
+    material_eval += pval*count(w_pawns)
+    material_eval -= pval*count(b_pawns)
+
+    for pawn in w_pawns
+        position_eval += PAWN_EVAL_TABLE[pawn]
+    end
+
+    for pawn in b_pawns
+        position_eval -= PAWN_EVAL_TABLE[65 - pawn]
+    end
+
+    eval = material_eval + position_eval
+end
+
+
+function evaluate_knights(board::Board)
+    w_knights = board[WHITEKNIGHT]
+    b_knights = board[BLACKKNIGHT]
+    pval = PVALS[2]
+
+    material_eval = 0
+    position_eval = 0
+
+    material_eval += pval*count(w_knights)
+    material_eval -= pval*count(b_knights)
+
+    for knight in w_knights
+        position_eval += KNIGHT_EVAL_TABLE[knight]
+    end
+
+    for knight in b_knights
+        position_eval -= KNIGHT_EVAL_TABLE[65 - knight]
+    end
+
+    eval = material_eval + position_eval
+end
+
+
+function evaluate_bishops(board::Board)
+    w_bishops = board[WHITEBISHOP]
+    b_bishops = board[BLACKBISHOP]
+    pval = PVALS[3]
+
+    material_eval = 0
+    position_eval = 0
+
+    material_eval += pval*count(w_bishops)
+    material_eval -= pval*count(b_bishops)
+
+    for bishop in w_bishops
+        position_eval += BISHOP_EVAL_TABLE[bishop]
+    end
+
+    for bishop in b_bishops
+        position_eval -= BISHOP_EVAL_TABLE[65 - bishop]
+    end
+
+    eval = material_eval + position_eval
+end
+
+
+function evaluate_rooks(board::Board)
+    w_rooks = board[WHITEROOK]
+    b_rooks = board[BLACKROOK]
+    pval = PVALS[4]
+
+    material_eval = 0
+    position_eval = 0
+
+    material_eval += pval*count(w_rooks)
+    material_eval -= pval*count(b_rooks)
+
+    for rook in w_rooks
+        position_eval += ROOK_EVAL_TABLE[rook]
+    end
+
+    for rook in b_rooks
+        position_eval -= ROOK_EVAL_TABLE[65 - rook]
+    end
+
+    eval = material_eval + position_eval
+end
+
+
+function evaluate_queens(board::Board)
+    w_queens = board[WHITEQUEEN]
+    b_queens = board[BLACKQUEEN]
+    pval = PVALS[5]
+
+    material_eval = 0
+    position_eval = 0
+
+    material_eval += pval*count(w_queens)
+    material_eval -= pval*count(b_queens)
+
+    for queen in w_queens
+        position_eval += QUEEN_EVAL_TABLE[queen]
+    end
+
+    for queen in b_queens
+        position_eval -= QUEEN_EVAL_TABLE[65 - queen]
+    end
+
+    eval = material_eval + position_eval
+end
+
+
+function evaluate_kings(board::Board)
+    w_king = board[WHITEKING]
+    b_king = board[BLACKKING]
+    pval = PVALS[6]
+
+    material_eval = 0
+    position_eval = 0
+
+    position_eval += KING_EVAL_TABLE[square(w_king)]
+    position_eval -= KING_EVAL_TABLE[65 - square(b_king)]
+
+    eval = material_eval + position_eval
+end
+
+
+function evaluate_pins(board::Board)
+    eval = 0
+
+    # switch turn and find all pins
+    board.turn = !board.turn
+    opp_pinned = findpins(board)
+    board.turn = !board.turn
+
+    if board.turn == WHITE
+        eval -= count(board.pinned) * 15
+        eval += count(opp_pinned) * 15
+    else
+        eval += count(board.pinned) * 15
+        eval -= count(opp_pinned) * 15
+    end
+
+    eval
 end
