eaa4cadbb1c2f0fb46bbc5b268caddd54b04c50b
J-Revell <11092843+J-Revell@users.noreply.github.com>
2020-07-13 22:41:37 +0100

    Windows compilation

--- deps/build.jl
+++ deps/build.jl
@@ -5,16 +5,20 @@ const builddir = (@__DIR__)
 isdir(builddir) || mkdir(builddir)
 
 const CFLAGS = `-std=c99 -O2 -Wall -D TB_NO_THREADS -fPIC -shared`
-input = builddir*"/tbprobe.c"
-output = builddir*"/tbprobe.so"
-
-@info "Setting flags and outputs complete."
-
-@info "Building fathom's tbprobe..."
-cmd = `gcc $(CFLAGS) -I.. $input -o $output`
+if Sys.iswindows()
+    input = builddir*"\\tbprobe.c"
+    output = builddir*"\\tbprobe.exe"
+    @info "Setting flags and outputs complete."
+    @info "Building fathom's tbprobe..."
+    cmd = `x86_64-w64-mingw32-gcc $(CFLAGS) -I.. $input -o $output`
+else
+    input = builddir*"/tbprobe.c"
+    output = builddir*"/tbprobe.so"
+    @info "Setting flags and outputs complete."
+    @info "Building fathom's tbprobe..."
+    cmd = `gcc $(CFLAGS) -I.. $input -o $output`
+end
 run(cmd)
-
-
 
 @info "Building config.jl..."
 file = open(builddir*"/config.jl", "w")
@@ -24,8 +28,8 @@ close(file)
 
 @info "Setting executable flag for AstellarnEngine.jl"
 cd("../src")
-run(`chmod a+x Astellarn.jl`)
+if Sys.islinux()
+    run(`chmod a+x Astellarn.jl`)
+end
 
 @info "Build finished."
-
-
